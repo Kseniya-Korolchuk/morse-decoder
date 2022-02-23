a6002c67c1e92b8c2731df42cbe8ae3684d1c5e1
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-02-23 23:36:46 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,8 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+
+    let array = expr.match( /.{1,10}/g );
+    let words = array.map(el => el.replace(/0/g, ' ').trimStart().replace(/ /g, '0'));
+    let morse = words.map(el => el.replace(/10/g, '.').replace(/11/g, '-').replace('**********', ' '));
+    let sentence = morse.map (el => {
+        for (let key in MORSE_TABLE) {
+          if (el == key) {
+            return MORSE_TABLE[key];
+          } 
+          else if (el == ' ') {
+            return el;
+          }
+        }
+    })
+    return sentence.join('');
+} 
 
 module.exports = {
     decode
